65c0308cc736f193cea600a6822e16680f9768af
Sven Titgemeyer <s.titgemeyer@titgemeyer-it.de>
2017-01-20 20:30:03 +0100

    add Cannon, enemy, and background

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public ShootingController shootingController;
+	void OnMouseDown() {
+		shootingController.didTapEnemy (gameObject);
+	}
+}
---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour {
+
+
+	public Transform cannonTip;
+
+	public void didTapEnemy(GameObject enemy){
+		shootObject (enemy);
+	}
+
+	private void shootObject(GameObject enemy){
+		Destroy (enemy);
+	}
+}
