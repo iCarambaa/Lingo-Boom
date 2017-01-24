58552d87b7f3d5545b361b65c66bc33ce5396f0e
Sven Titgemeyer <s.titgemeyer@titgemeyer-it.de>
2017-01-24 15:53:43 +0100

    make the cannon follow the enemy ship

--- Unity/Assets/Scripts/ShootingController.cs
+++ Unity/Assets/Scripts/ShootingController.cs
@@ -6,12 +6,26 @@ public class ShootingController : MonoBehaviour {
 
 
 	public Transform cannonTip;
+	public Transform cannon;
+	public Transform cannonMount;
 
-	public void didTapEnemy(GameObject enemy){
-		shootObject (enemy);
+	public GameObject target;
+
+	public void Update(){
+		updateRotation ();
 	}
 
-	private void shootObject(GameObject enemy){
-		Destroy (enemy);
+	private void updateRotation(){
+		if (target) {
+			Vector3 enemyPosition = target.transform.position;
+			Vector3 stationPosition = cannonMount.position;
+
+			Vector3 direction = enemyPosition - stationPosition;
+			Vector3 upVector = Vector3.up;
+
+			float angle = Vector3.Angle (upVector, direction);
+			//the pivot point is set when importing the sprite
+			Quaternion rotation = Quaternion.AngleAxis (angle, new Vector3 (0, 0, 1));
+		}
 	}
 }
