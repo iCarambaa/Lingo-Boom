d1cb2ba3e3ea4e54be416a4d941d14c115dd7223
Sven Titgemeyer <s.titgemeyer@titgemeyer-it.de>
2017-01-25 16:08:49 +0100

    add infinite scrolling background

---
+++ Unity/Assets/ScrollingBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public float scrollingSpeed = 1.0f;
+	public float scrollBorder = 13.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cPosition = transform.position;
+		cPosition.y -= scrollingSpeed * Time.deltaTime;
+		cPosition.y = cPosition.y % scrollBorder;
+		transform.position = cPosition;
+
+	}
+}
