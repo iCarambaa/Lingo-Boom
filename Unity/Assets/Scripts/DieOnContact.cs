7dc061fc647203188db97d7078aba2cd7749f49b
Stella Coumbassa <scoumbassa@gmail.com>
2017-01-25 01:28:09 +0100

    added projectile shooting, destroying enemy on contact

--- Unity/Assets/Scripts/DieOnContact.cs
+++ Unity/Assets/Scripts/DieOnContact.cs
@@ -7,8 +7,16 @@ public class DieOnContact : MonoBehaviour {
 
 	public GameObject explosionPrefab;
 
+
 	void OnMouseDown(){
 		Instantiate (explosionPrefab, transform.position, transform.rotation);
 		Destroy (gameObject);
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //destroy object when projectile collides with it
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
 }
---
+++ Unity/Assets/Scripts/Shoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    //where the projectile comes from
+    public GameObject cannonTip;
+    public GameObject projectile;
+    public float projectileForce;
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //modify later - to button press
+    private void OnMouseDown()
+    {
+        //instantiate projectile
+        GameObject tempProjectile;
+        tempProjectile = Instantiate(projectile, cannonTip.transform.position, cannonTip.transform.rotation) as GameObject;
+
+        //target position
+        Vector2 position = target.transform.position - cannonTip.transform.position;
+
+        //control the rigidbody component of projectile to shoot it
+        Rigidbody2D tempRigidbody;
+        tempRigidbody = tempProjectile.GetComponent<Rigidbody2D>();
+
+        //shoot the projectile
+        tempRigidbody.velocity = position * 10;
+
+        //after 2 second projectile is automatically destroyed if not used
+        Destroy(tempProjectile, 2.0f);
+    }
+}
